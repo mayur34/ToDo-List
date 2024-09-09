b935873bd18f627f4f4411cce7322509d36fa754
Mayur177 <mayur.yadav@eduvanz.com>
2024-09-09 15:20:48 +0530

    code refactor structure

--- Backend/app.js
+++ Backend/app.js
@@ -5,19 +5,56 @@ const port = 4000;
 
 app.use(express.json());
 
-app.get("/api/v1/task", (req, res) => {
+const getAllTask = (req, res) => {
   res.status(200).json({
     status: "success",
     data: "temp",
   });
-});
+};
 
-app.post("/api/v1/task", (req, res) => {
+const createTask = (req, res) => {
   console.log(req.body);
   //Send Id
 
   res.send("Done");
-});
+};
+
+const updateTask = (req, res) => {
+  console.log(req.body);
+  //will update a specific property
+  res.send("Done");
+};
+
+const deleteTask = (req, res) => {
+  // console.log(req.body);
+  //will update a specific property
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+
+const getTask = (req, res) => {
+  console.log(req.body.id);
+  //will update a specific property
+  res.status(200).json({
+    status: "success",
+    data: "1 data",
+  });
+};
+
+// app.get("/api/v1/task", getAllTask);
+
+// app.get("/api/v1/task/:id", getTask);
+
+// app.post("/api/v1/task", createTask);
+
+// app.patch("/api/v1/task/:id", updateTask);
+
+// app.delete("/api/v1/task/:id", deleteTask);
+
+app.route("/api/v1/task").get(getAllTask).post(createTask);
+app.route("/api/v1/task/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);

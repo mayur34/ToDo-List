6290d78c848634824f820d08da62be629a32320d
mayur34 <mayurinfo707@gmail.com>
2025-03-01 03:47:06 +0530

    backend changes

--- Backend/controllers/TaskControllers.js
+++ Backend/controllers/TaskControllers.js
@@ -1,3 +1,5 @@
+const taskModel = require('./../models/taskModel');
+
 //This is a middleware function to check if data is empty in post method
 exports.checkBody = (req, res, next) => {
   console.log(req.body.data, "mayur");
@@ -19,41 +21,161 @@ exports.CheckIDForParamMiddleware = (req, res, next, val) => {
   console.log("checked id", val);
   next();
 };
-exports.getAllTask = (req, res) => {
+
+
+exports.getAllTask = async (req, res) => {
+ try{
+//  1.Filtering
+
+  //First way
+  // const allTask = await taskModel.find({
+  //   option:"low",
+  //   title:"asdds"
+  // })
+
+  //2nd way
+  // const allTask= await taskModel.find().where('option').equals('low').where('title').equals('asdds')
+  
+  //final way
+  //const allTask =await taskModel.find(req.query)
+  
+ //excluding parameter
+ //Build Query
+ const excludeObj = {...req.query}
+ const excludeFields=['limit','sort','page','fields']
+
+ excludeFields.forEach((el)=> delete excludeObj[el])
+
+
+//  const query =await taskModel.find(excludeObj)
+ 
+//  const allTask =await query
+
+//2.Advance filter
+// http://localhost:4000/api/v1/task?title[gte]=asdds we get console {title:{gte:'asdds'}} just an example
+let queryStr = JSON.stringify(excludeObj)
+queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>`$${match}`)
+console.log(JSON.parse(queryStr)) //we are converting it to  {title:{$gte:'asdds'}} we are adding $ to it so ooperator can work
+let query = taskModel.find(JSON.parse(queryStr))
+ 
+//3 Sorting (first exclude the field as above,why because it will try  will attempt to match these parameters as fields within documents, which can cause unexpected behavior (like returning an empty array) then do other thing as below) 
+//using sort
+
+//for single sort
+// if(req.query.sort){
+//   // console.log(req.query.sort)
+//   query=query.sort(req.query.sort)
+
+// }
+
+//Sorting by multiple values
+
+// if(req.query.sort){
+//   const sortBy=req.query.sort.split(',').join('')
+//   console.log(sortBy,'why d')
+//   query=query.sort(sortBy)
+//   console.log(query,'hu')
+// }else{
+//   query=query.sort('-createdAt')
+// }
+
+//4 Limit(to limit the data) using fields
+if(req.query.fields){
+  const sortByFields = req.query.fields.split(',').join('')
+  query=query.select(sortByFields)
+}else{
+  query=query.select('-__v')
+}
+
+//console.log(query,'tty')
+// console.log(query)
+ const allTask=await query
+  console.log(allTask,'ssk')
   res.status(200).json({
-    requesAt: req.requestTime,
     status: "success",
-    data: "temp",
+    data: allTask
   });
+}
+catch(err){
+  res.status(400).json({
+    status:"fail",
+    message:err
+  })
+}
 };
 
-exports.createTask = (req, res) => {
-  console.log(req.body);
-  //Send Id
+exports.createTask = async (req, res) => {
+//one way of to add 
+  //  const newTask = new taskModel(req.body);
+//  await newTask.save();
+try{
+const newTask= await taskModel.create(req.body)
 
-  res.send("Done");
+res.status(200).json({
+  status:"success",
+  data:{
+    task:newTask
+  }
+})
+}catch(err){
+  res.status(400).json({
+    status:"fail",
+    message:err
+  })
+}
+ 
 };
 
-exports.updateTask = (req, res) => {
-  console.log(req.body);
-  //will update a specific property
-  res.send("Done");
+
+exports.updateTask = async(req, res) => {
+try{
+  const task = await taskModel.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+    runValidators:true
+  })
+
+  res.status(200).json({
+    status:"success",
+    task:task
+  })
+  }
+  catch(err){
+    res.status(400).json({
+      status:"fail",
+      message:err
+    })
+  }
 };
 
-exports.deleteTask = (req, res) => {
-  // console.log(req.body);
-  //will update a specific property
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTask = async(req, res) => {
+  try{
+  const task = await taskModel.findByIdAndDelete(req.params.id)
+  res.status(200).json({
+    status:"success",
+    message:"Deleted Successfully"
+  })
+  }
+  catch(err){
+    res.status(400).json({
+      status:"fail",
+      message:err
+    })
+  }
 };
 
-exports.getTask = (req, res) => {
-  console.log(req.body.id);
+exports.getTask = async (req, res) => {
+try{
+  const task= await taskModel.findById(req.params.id);
+  //await taskModel.findOne({_id:req.params.id}); another way
   //will update a specific property
   res.status(200).json({
     status: "success",
-    data: "1 data",
+    data: task,
   });
+}catch(err){
+  res.status(400).json({
+    status:"fail",
+    message:err
+  })
+}
 };
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,26 @@
+const dotenv=require('dotenv')
+
+//Providing path to env file
+dotenv.config({path:'./config.env'})
+
+const mongoose=require('mongoose')
 const app = require("./app");
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+const DB= process.env.DATABASE_URL.replace('<db_password>',process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology: true
+}).then(con=>{
+   console.log(con.connections)
+  console.log("Connection SuccessFul")
+})
+
+
 
 //Server
 app.listen(port, () => {
